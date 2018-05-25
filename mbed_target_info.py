d95bfa7eeac8ef7756b4af2b4f64684b0a902c11
maclobdell <maclain.lobdell@arm.com>
2018-05-25 12:45:20 -0500

    update data output format

--- mbed_target_info.py
+++ mbed_target_info.py
@@ -12,26 +12,40 @@ from tools.targets import set_targets_json_location  #for changing location of t
 
 def main():
 
-    device_data_json = {}
-    
+    device_data_array = []
     device_has_template = { 
         'FLASH': 'x', 'RTC': 'x', 'SPI': 'x', 'I2C': 'x', 'TRNG': 'x', 'SLEEP': 'x'
     }
     
     set_targets_json_location("C://Users//maclob01//Documents//Projects//TARGET_PARSING//mbed-target-parsing//example_targets.json")
 
-    device_data_json["DATE"] = "2018/04/25 21:22"
-    device_data_json["MBED-OS-VER"] = "3.8.2"
+
     
-    for target in TARGETS:        
+    for target in TARGETS:  
+        
+        #construct an array of targets here
+        device_data_entry = {}
+               
+        device_data_entry["name"] = target.name
+        
+        device_data_description = {}
+        
+        device_data_description["ver"] = "5.9.1"
+        device_data_description["name"] = target.name
+        device_data_description["date"] = "May 25, 2018" 
+                             
         for feature in target.device_has:
             for template_feature in device_has_template:
                 if feature == template_feature:
                     device_has_template[feature] = 'YES' 
 
-        device_data_json[target.name] = json.dumps(device_has_template)     
+        device_data_description["target_data"] = json.dumps(device_has_template)     
 
-    s = json.dumps(device_data_json)    
+        device_data_entry["description"] = device_data_description
+
+        device_data_array.append(device_data_entry)
+
+    s = json.dumps(device_data_array)    
     with open (".//device_data_output.json", "w") as f:
         f.write(s)
         f.close()
